a34e86e95faebda9b1fd0b02252b2c1c60196f93
liron-dev <liron6654@gmail.com>
2026-01-27 16:25:02 +0200

    docker-compose

--- db/init.sql
+++ db/init.sql
@@ -10,5 +10,5 @@ CREATE TABLE cars (
 
 INSERT INTO cars (name, hp, engine, description, image, tags) 
 VALUES 
-('Porsche 911 GT3 RS', 518, '4.0L Flat-6', 'The pinnacle of 911 performance. Built for the track.', 'images/porsche.jpg', 'German,Track-Tool,Naturally Aspirated'),
-('DeLorean DMC-12', 130, '2.85L V6', 'The stainless steel icon. Flux capacitor included.', 'images/delorean.jpg', 'Movie Icon,Stainless Steel,Gull-wing');
+('Porsche 911 GT3 RS', 518, '4.0L Flat-6', 'The pinnacle of 911 performance. Built for the track.', 'images/porsche.jpeg', 'German,Track-Tool,Naturally Aspirated'),
+('DeLorean DMC-12', 130, '2.85L V6', 'The stainless steel icon. Flux capacitor included.', 'images/delorean.jpeg', 'Movie Icon,Stainless Steel,Gull-wing');
