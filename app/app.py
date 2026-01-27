c97231179f87de30cb31cc387181ac905d749971
liron-dev <liron6654@gmail.com>
2026-01-27 16:07:49 +0200

    added db and app function

--- app/app.py
+++ app/app.py
@@ -1,7 +1,18 @@
+import os
+import psycopg2
 from flask import Flask, render_template
 
 # Create the Flask application instance
 app = Flask(__name__)
+
+def get_db_connection():
+    conn = psycopg2.connect(
+        host=os.environ.get('DB_HOST', 'db'), # Default to 'db' (service name)
+        database=os.environ.get('DB_NAME', 'cars_db'),
+        user=os.environ.get('DB_USER', 'postgres'),
+        password=os.environ.get('DB_PASS', 'password')
+    )
+    return conn
 
 # Temporary Database:
 CARS_DATA = [
@@ -23,10 +34,26 @@ CARS_DATA = [
     }
 ]
 
-# Route the home page to this function
 @app.route('/')
 def index():
-    return render_template('index.html', cars=CARS_DATA)
+    conn = get_db_connection()
+    cur = conn.cursor()
+    cur.execute('SELECT name, hp, engine, description, image, tags FROM cars;')
+    rows = cur.fetchall()
+    cars = []
+    for row in rows:
+        cars.append({
+            "name": row[0],
+            "hp": row[1],
+            "engine": row[2],
+            "description": row[3],
+            "image": row[4],
+            "tags": row[5].split(",")
+        })
+
+    cur.close()
+    conn.close()
+    return render_template('index.html', cars=cars)
 
 if __name__ == '__main__':
     # app.run() starts the local server
---
+++ db/init.sql
@@ -0,0 +1,14 @@
+CREATE TABLE cars (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(100),
+    hp INTEGER,
+    engine VARCHAR(50),
+    description TEXT,
+    image VARCHAR(100),
+    tags TEXT
+);
+
+INSERT INTO cars (name, hp, engine, description, image, tags) 
+VALUES 
+('Porsche 911 GT3 RS', 518, '4.0L Flat-6', 'The pinnacle of 911 performance. Built for the track.', 'images/porsche.jpg', 'German,Track-Tool,Naturally Aspirated'),
+('DeLorean DMC-12', 130, '2.85L V6', 'The stainless steel icon. Flux capacitor included.', 'images/delorean.jpg', 'Movie Icon,Stainless Steel,Gull-wing');
